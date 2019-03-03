9bb577e77e101cdbb53d38dde6487341fb70ab94
lsikora <sikora.lukasz.sl@gmail.com>
2019-03-03 19:01:09 +0100

    Add simultaneous support for JUnit 5 and 4 with pure gradle

---
+++ src/test/java/io/github/lsikora/imports/JUnit4Simple.java
@@ -0,0 +1,10 @@
+package io.github.lsikora.imports;
+
+import org.junit.Test;
+
+public class JUnit4Simple {
+
+    @Test
+    public void testJUnit4IsWorking() {
+    }
+}
---
+++ src/test/java/io/github/lsikora/imports/JUnit5Simple.java
@@ -0,0 +1,11 @@
+package io.github.lsikora.imports;
+
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5Simple {
+
+    @Test
+    void testJUnit5IsWorking(){
+    }
+}
