3c6ae5ae57bef86d21f00ea61d37bca770fee48b
lsikora <sikora.lukasz.sl@gmail.com>
2019-03-09 14:44:48 +0100

    Add simple test cases

--- src/test/java/io/github/lsikora/imports/JUnit4Simple.java
+++ src/test/java/io/github/lsikora/imports/JUnit4Simple.java
@@ -2,9 +2,12 @@ package io.github.lsikora.imports;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class JUnit4Simple {
 
     @Test
     public void testJUnit4IsWorking() {
+        assertTrue(true);
     }
 }
--- src/test/java/io/github/lsikora/imports/JUnit5Simple.java
+++ src/test/java/io/github/lsikora/imports/JUnit5Simple.java
@@ -3,9 +3,12 @@ package io.github.lsikora.imports;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class JUnit5Simple {
 
     @Test
-    void testJUnit5IsWorking(){
+    public void testJUnit5IsWorking() {
+        assertTrue(true);
     }
 }
