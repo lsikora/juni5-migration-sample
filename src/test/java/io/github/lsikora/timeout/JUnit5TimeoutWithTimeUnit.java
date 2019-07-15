3ad6e72a92acd51414b9f48b9c9b3ccf0af3d197
lsikora <sikora.lukasz.sl@gmail.com>
2019-07-15 23:10:43 +0200

    Add new @Timeout annotation usages

--- src/test/java/io/github/lsikora/timeout/JUnit5TimeoutWithTimeUnit.java
+++ src/test/java/io/github/lsikora/timeout/JUnit5TimeoutWithTimeUnit.java
@@ -3,22 +3,17 @@ package io.github.lsikora.timeout;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
-import java.time.Duration;
-
 import static java.lang.Thread.sleep;
-import static java.time.temporal.ChronoUnit.MILLIS;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 //todo explain new annotation that works on classes and methods
-@Timeout(4)
-class JUnit5Timeout {
-    @Test
-    void willTimeoutWithAssertion() {
-        assertTimeout(
-                Duration.of(100, MILLIS),
-                ()-> sleep(200));
-    }
-
+//  add note that this timeout is better than havin rule to enforce global
+//  timeout in JUnit 4.  Make pun that this annotation declaration is to long
+//  since it requires explicit miliseconds specification and JUnit 5 uses
+//  seconds by default. Why so large timeouts? Why TimeUnit.MILISECONDS
+//  instead of TimeUnit.MILIS which is more coherent with Java APIs
+@Timeout(value = 4, unit = MILLISECONDS)
+class JUnit5TimeoutWithTimeUnit {
     //todo make slide with pun on how JUnit 5 uses seconds where most API's
     // use miliseconds
 
@@ -27,9 +22,8 @@ class JUnit5Timeout {
     // a second it should be max few miliseconds long
 
     //todo does test will not timeout due to no override for default time unit
-    // those tests are expected to fail. But they do not.
     @Test
-    @Timeout(2)
+    @Timeout(value = 2, unit = MILLISECONDS)
     void willTimeoutWithMethodTimeout() throws InterruptedException {
         sleep(3);
     }
