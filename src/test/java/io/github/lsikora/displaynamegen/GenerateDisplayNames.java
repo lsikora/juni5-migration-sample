902869dafd81eb1941fb052c4ee79452a1dbd294
lsikora <sikora.lukasz.sl@gmail.com>
2020-02-22 19:32:07 +0100

    Ask JUnit 5 team for support https://github.com/junit-team/junit5/issues/1595

--- src/test/java/io/github/lsikora/displaynamegen/GenerateDisplayNames.java
+++ src/test/java/io/github/lsikora/displaynamegen/GenerateDisplayNames.java
@@ -1,14 +1,20 @@
 package io.github.lsikora.displaynamegen;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 
+/*This test will create human readable description based on snake case.
+Sadly camelcase version of DisplayNameGenerator is being hold when it should
+pass given it covers most common cases https://github.com/junit-team/junit5/issues/1595*/
 @DisplayNameGeneration(ReplaceUnderscores.class)
+@DisplayName("This test shows that projects which use snake case can already " +
+        "have human readable test output")
 class GenerateDisplayNames {
 
     @Test
-    void should_Be_Displayed_With_Generated_Display_Name() {
+    void this_test_shows_that_JUnit5_converts_snake_case_to_human_readable_with_correct_setup() {
     }
 }
