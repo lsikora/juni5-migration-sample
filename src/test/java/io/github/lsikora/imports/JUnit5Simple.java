f5985c49d70fe62cfcd1ed835c03f403a4ac6808
lsikora <sikora.lukasz.sl@gmail.com>
2020-02-23 14:01:30 +0100

    Make imports self explanatory

--- src/test/java/io/github/lsikora/imports/JUnit5Simple.java
+++ src/test/java/io/github/lsikora/imports/JUnit5Simple.java
@@ -1,10 +1,12 @@
 package io.github.lsikora.imports;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("This test will be executed if JUnit 5 is properly configured")
 class JUnit5Simple {
 
     @Test
