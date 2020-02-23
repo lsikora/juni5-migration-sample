3f64b206ebef380f3d92d7559ac21d312a1695be
lsikora <sikora.lukasz.sl@gmail.com>
2020-02-23 16:16:36 +0100

    Add embedded execution conditions

--- src/test/java/io/github/lsikora/conditional/executioncondition/WillNotRunDueToConditionAnnotation.java
+++ src/test/java/io/github/lsikora/conditional/executioncondition/WillNotRunDueToConditionAnnotation.java
@@ -2,6 +2,11 @@ package io.github.lsikora.conditional.executioncondition;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import static org.junit.jupiter.api.condition.JRE.JAVA_9;
+import static org.junit.jupiter.api.condition.OS.MAC;
+import static org.junit.jupiter.api.condition.OS.SOLARIS;
 
 @DisplayName("This test shows how to filter test execution based on " +
         "ExecutionCondition feature in JUnit 5")
@@ -15,5 +20,15 @@ class WillNotRunDueToConditionAnnotation {
     void disableOnConditionAnnotation() {
         throw new RuntimeException("This exception must never be thrown if " +
                 "execution conditions work ;)");
+    }
+
+    @EnabledOnOs({SOLARIS, MAC})
+    @EnabledForJreRange(min = JAVA_9)
+    @EnabledIfSystemProperty(named = "os.arch", matches = ".*64.*")
+    @DisabledIfEnvironmentVariable(named = "os.arch", matches = ".*64.*")
+    @Test
+    @DisplayName("This test may be run depending on embedded execution " +
+            "conditions of JUnit 5")
+    void embedded() {
     }
 }
