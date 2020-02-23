597a9fc5fdf45b9784394196d404860ff04f2f7e
lsikora <sikora.lukasz.sl@gmail.com>
2020-02-23 16:09:27 +0100

    Make timeout self explanatory

--- src/test/java/io/github/lsikora/timeout/JUnit4Timeout.java
+++ src/test/java/io/github/lsikora/timeout/JUnit4Timeout.java
@@ -6,17 +6,25 @@ import org.junit.rules.Timeout;
 
 import static java.lang.Thread.sleep;
 
+/*This test shows different approaches to make test time out in JUnit 4*/
 public class JUnit4Timeout {
     @Rule
     public Timeout timeout = Timeout.millis(100);
 
     @Test
-    public void willTimeoutGlobally() throws InterruptedException{
+    public void willTimeoutDueToTimeoutRule() throws InterruptedException {
         sleep(200);
     }
 
+    /*Timeout parameter in test is in miliseconds, which works nicely with
+    Thread API*/
     @Test(timeout = 50)
-    public void willTimeout() throws InterruptedException{
+    public void willTimeoutDueToTestAnnotationParam() throws InterruptedException {
         sleep(200);
+    }
+
+    @Test
+    public void willNotTimeOut() throws InterruptedException {
+        sleep(50);
     }
 }
--- src/test/java/io/github/lsikora/timeout/JUnit5Timeout.java
+++ src/test/java/io/github/lsikora/timeout/JUnit5Timeout.java
@@ -1,41 +1,50 @@
 package io.github.lsikora.timeout;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
-import java.time.Duration;
-
 import static java.lang.Thread.sleep;
+import static java.time.Duration.of;
 import static java.time.temporal.ChronoUnit.MILLIS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
-//todo explain new annotation that works on classes and methods
 @Timeout(4)
+@DisplayName("This test shows different approaches to make test time out in " +
+        "JUnit 5:")
 class JUnit5Timeout {
     @Test
-    void willTimeoutWithAssertion() {
+    @DisplayName("Using assertion without interrupting execution")
+    void assertionTimeout() {
         assertTimeout(
-                Duration.of(100, MILLIS),
-                ()-> sleep(200));
-    }
-
-    //todo make slide with pun on how JUnit 5 uses seconds where most API's
-    // use miliseconds
-
-    //todo will not timeout because timeout is in seconds by default. Which
-    // sucks because unit tests should be fast and not unit test should last
-    // a second it should be max few miliseconds long
-
-    //todo does test will not timeout due to no override for default time unit
-    // those tests are expected to fail. But they do not.
-    @Test
-    @Timeout(2)
-    void willTimeoutWithMethodTimeout() throws InterruptedException {
-        sleep(3);
+                of(100, MILLIS),
+                () -> sleep(200)
+        );
     }
 
     @Test
-    void willTimeoutWithGlobalTimeoutOnClassLevel() throws InterruptedException {
+    @DisplayName("Using assertion with interrupting execution")
+    void assertionTimeoutPreemptively() {
+        assertTimeoutPreemptively(
+                of(100, MILLIS),
+                () -> sleep(200)
+        );
+    }
+
+    /*This method is classic example of RTFM. Default unit for timeout is
+    seconds. Thread sleeps for milliseconds so test will not timeout*/
+    @Test
+    @DisplayName("Using @Timeout on class")
+    void globalTimeout() throws InterruptedException {
         sleep(5);
+    }
+
+    @Test
+    @Timeout(value = 2, unit = MILLISECONDS)
+    @DisplayName("Using @Timeout on method")
+    void annotatedTimeout() throws InterruptedException {
+        sleep(3);
     }
 }
